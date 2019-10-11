9be8fa5f189740282e8b252f2c4e438ddb32821a
krikaz <pascal.bou@protonmail.com>
2019-10-11 11:41:31 +0200

    robot works until the last swap

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -109,26 +109,31 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        if not self.light_is_on():
-            if not self.can_move_right():
-                # self.set_light_off()
-                return
-            else:
-                self.swap_item()
-                self.set_light_on()
-
-        while self.can_move_right():
-            self.move_right()
-            if self.compare_item() > 0:
-                self.swap_item()
+        self.swap_item()
+        self.set_light_on()
 
         while self.light_is_on():
-            self.move_left()
-            if self.compare_item() == None:
-                self.swap_item()
+            if not self.can_move_right():
                 self.set_light_off()
-                # self.sort()
+                return
 
+            while self.can_move_right():
+                self.move_right()
+                # if not self.can_move_right():
+                #     self.set_light_off()
+                #     return
+                if self.compare_item() == None or self.compare_item() > 0:
+                    self.swap_item()
+
+            while self.compare_item() != None:
+                self.move_left()
+                if self.compare_item() == None:
+                    self.swap_item()
+                    # self.set_light_off()
+                    # self.sort()
+                    
+            print(self._position)
+            print(self._list)
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
